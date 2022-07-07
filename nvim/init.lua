408fbf71e900a4921146269c1ac549bb39bd6021
Dylan Simowitz <git@dylan.simowitz.com>
2022-07-07 08:45:12 -0700

    use eslint_d instead of eslint

--- nvim/init.lua
+++ nvim/init.lua
@@ -69,7 +69,7 @@ return {
       vale["filetypes"] = { "markdown", "tex", "asciidoc", "html" }
       config.sources = {
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.eslint_d,
         vale
       }
       config.on_attach = function(client)
