d5596dc9d2f8b1ceaae0af3d89fe3c2ea012a04c
Dylan Simowitz <dylan@daisy.health>
2023-03-27 23:12:40 -0700

    fix eslint and prettier interaction

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,20 @@
 return {
-	{
-		"nvim-lspconfig",
-		opts = {
-			diagnostics = {
-				virtual_text = false,
-			},
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = { eslint = {} },
+		diagnostics = {
+			virtual_text = false,
+		},
+		setup = {
+			eslint = function()
+				require("lazyvim.util").on_attach(function(client)
+					if client.name == "eslint" then
+						client.server_capabilities.documentFormattingProvider = true
+					elseif client.name == "tsserver" then
+						client.server_capabilities.documentFormattingProvider = false
+					end
+				end)
+			end,
 		},
 	},
 }
